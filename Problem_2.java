f4470840841e65dd67bf36ef5009e360522801e9
Zeel Satasiya <115857227+ZeelSatasiya@users.noreply.github.com>
2025-03-17 15:17:29 -0700

    Update Problem_2.java

--- Problem_2.java
+++ Problem_2.java
@@ -5,24 +5,53 @@
 
 //658. Find K Closest Elements
 
+// Binary Search
+// TC: O(log(n - k))
+// SC: O(1)
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        if(arr == null || arr.length == 0) return new ArrayList<>();
-        int n = arr.length;
+        List<Integer> result = new ArrayList<>();
+        if(arr == null || arr.length == 0 || k == 0) return result;
         int left = 0;
-        int right = n - 1;
-        while(right - left + 1 > k){
-            if(Math.abs(x - arr[left]) > Math.abs(x - arr[right])){
-                left++;
+        int right = arr.length - k;
+        while(left < right){
+            int mid = left + (right - left) / 2;
+            int distS = x - arr[mid];
+            int distE = arr[mid + k] - x;
+            if(distS > distE){
+                left = mid + 1;
             }
             else{
-                right--;
+                right = mid;
             }
         }
-        List<Integer> result = new ArrayList<>();
-        for(int i = left; i <= right; i++){
+        for(int i = left; i < left + k; i++){
             result.add(arr[i]);
         }
         return result;
     }
 }
+
+// // 2 pointer
+// // TC: O(n)
+// // SC: O(1)
+// class Solution {
+//     public List<Integer> findClosestElements(int[] arr, int k, int x) {
+//         List<Integer> result = new ArrayList<>();
+//         if(arr == null || arr.length == 0 || k == 0) return result;
+//         int left = 0;
+//         int right = arr.length - 1;
+//         while((right - left + 1) > k){
+//             if(Math.abs(arr[left] - x) > Math.abs(arr[right] - x)){
+//                 left++;
+//             }
+//             else{
+//                 right--;
+//             }
+//         }
+//         for(int i = left; i <= right; i++){
+//             result.add(arr[i]);
+//         }
+//         return result;
+//     }
+// }
